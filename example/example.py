adba28046f5e275db82bb1fb0806fa7dd07db69d
jsmolka <jsmolka@pm.me>
2017-07-25 22:00:49 +0200

    easier example

--- example/example.py
+++ example/example.py
@@ -1,7 +1,7 @@
 from maze import *
 
 m = Maze()  # Create maze object
-m.create(25, 25, Algorithm.Create.BACKTRACKING)  # Create a maze
-m.save_maze_as_png()  # Save the maze as maze.png
-m.solve(0, 0, Algorithm.Solve.BACKTRACKING)  # Solve the maze from top left to bottom right
-m.save_solution_as_png()  # Save the solution as png
+m.create(25, 25, Algorithm.Create.BACKTRACKING)  # Create maze
+m.save_maze_as_png()  # Save maze as maze.png
+m.solve((0, 0), (24, 24), Algorithm.Solve.BACKTRACKING)  # Solve maze from top left to bottom right
+m.save_solution_as_png()  # Save the solution as solution.png
