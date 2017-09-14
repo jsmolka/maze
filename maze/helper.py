7d3c7a242704a9c95fa9d9d432fd062f9bb6c14c
jsmolka <jsmolka@pm.me>
2017-09-15 00:14:34 +0200

    greatly improve scaling speed

--- maze/helper.py
+++ maze/helper.py
@@ -34,15 +34,8 @@ def upscale(maze, scale):
         maze = np.array(maze)  # Make sure maze is a numpy array
     if scale <= 1:
         return maze
-    row_count, col_count = len(maze), len(maze[0])
-    result = np.zeros((scale * row_count, scale * col_count, 3), dtype=np.uint8)
-
-    for x in range(0, row_count):
-        for y in range(0, col_count):
-            for i in range(0, scale):
-                for j in range(0, scale):
-                    result[scale * x + i, scale * y + j] = maze[x, y]
-    return result
+    else:
+        return maze.repeat(scale, axis=0).repeat(scale, axis=1)
 
 
 def get_scale(maze):
@@ -60,11 +53,5 @@ def downscale(maze):
     scale = get_scale(maze)
     if scale <= 1:
         return maze
-
-    row_count, col_count = len(maze) // scale, len(maze[0]) // scale
-    result = np.zeros((row_count, col_count, 3), dtype=np.uint8)
-
-    for x in range(0, row_count):
-        for y in range(0, col_count):
-            result[x, y] = maze[x * scale, y * scale]
-    return result
+    else:
+        return maze[::scale, ::scale]
